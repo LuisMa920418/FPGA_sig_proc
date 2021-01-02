5533b5ab2b6ec553b9f9214d483a87117e191259
«Luis» <lptelecom92@gmail.com>
2021-01-02 23:40:26 +0100

    c_polyphase for two branches done, for 1 branch questions to answer still

--- vivado_project/design/include/c_polyphase.hpp
+++ vivado_project/design/include/c_polyphase.hpp
@@ -20,6 +20,8 @@
 #include "c_filter.hpp"
 #include "rnd.h"
 
+
+
 using namespace sp_rnd;
 
 // =====================================================================================
@@ -31,16 +33,20 @@ template<int size, typename t_data, typename t_coef, typename t_acc, int shift, 
 class c_polyphase {
 
 	enum {
-		nb_branch   = 2,
+		nb_branch   = 2,  // #toask what is this parameter for? how is it used? is it for controlling the decimator factor?
 		size_filter = size/nb_branch
 	};
+   
 
-	// Branch 1 : Symmetric filter
-	// Branch 2 : Standard filter
+	
 
-	// ...
-	// ...
+		// nb_branch = 2 : 2 Symmetric filters  having coef from coef1 and coef2
+		c_filter<symmetric,t_data,t_coef,t_acc,size_filter> instance_filter_sym_branch1,instance_filter_sym_branch2;//instance for symetric filter
 
+		 // nb_branch =  1 : Standard filter  // #toask is the standard filter going to be used???
+    c_filter<standard,t_data,t_coef,t_acc,size_filter> instance_filter_std;    //instance for std filter
+	
+   
 	t_acc d_acc_poly;
 	int   cpt;
 
@@ -56,10 +62,64 @@ class c_polyphase {
 		t_data d_in;
 		t_acc  d_acc_filter;
 		t_out  d_out;
+        
 
+       /*
+	   cpt in range 0 to 1-> case two branches
+	   cpt always 0 -> case one branch 
+	   */
+		
+		
 		d_in = in.read();
+        
+		switch (nb_branch)
+		{
+			case 2:
+		    //case polyphase with two branches
+			if (cpt==0)
+			{
+				instance_filter_sym_branch1.process(d_in,coef_1, d_acc_filter);
+			}
+			else
+			//case cpt ==1
+			{
+				/*
+				#toask is this equivalent for the filter using coef_2?
+				instance_filter_branch2.shift(d_in);  //store the data
+				int size_div2 = instance_filter_branch2.size_div2; //get the constant size_div2
+				//get the data
+				t_data a =instance_filter_branch2.reg[size_div2];  
+				//get the coeficient
+				t_data b = coef_2[size_div2]; 
+				int coef_int = b.to_int();
+				//get the number of shifting
+				shift_times = log2(coef_int);  //how to get this??
+				//mshift
+				d_acc_filter.real = a.real<<shift_times;
+				d_acc_filter.imag = a.imag<<shift_times;
+*/
+				instance_filter_sym_branch2.process(d_in,coef_2,d_acc_filter);
+			}
+			d_acc_poly +=d_acc_filter;
+			break;
+		
+		default:
+		    //otherwise case nb_branch=1 for standard filter
+			//#toask coefficients parameters to use in case std_filter is used
+	        //create std filter coef from sym coef ? is that necessary for this project?
+		    t_coef coef_std;  //answer to know???
 
-		// ...
+			instance_filter_std.process(d_in,coef_std, d_acc_filter);
+			if (cpt==0)
+			{
+				d_acc_poly = d_acc_filter;
+			}
+		
+			break;
+		}
+		
+        
+		
 
 		// ------------------------------------------------------
 		// Function : "d_out = round(d_acc_poly)"
@@ -69,8 +129,8 @@ class c_polyphase {
 
 
 	 //shift right "shift" times(divission by 2^"shift"), d_acc_poly and store the return int value in d_out
-	 //sum(abs(filter_coefficients))=1286;
-	 //is this shifting the unity gain filter(fir filter with integer values) conversion?? yes, not why?
+	 //sum(abs(filter_coefficients))=1286~=2^10 = 1024
+	 //is this shifting the unity gain filter conversion??(fir filter with integer values) 
 		rnd<shift>(d_acc_poly.real,d_out.real); 
 		rnd<shift>(d_acc_poly.imag,d_out.imag);
 
@@ -80,7 +140,10 @@ class c_polyphase {
 
 		if(cpt==nb_branch-1) {
 
-			// ...
+			
+			d_acc_poly=0;
+			out.write(d_out);
+			cpt=0;
 
 		} else {
 			cpt++;
