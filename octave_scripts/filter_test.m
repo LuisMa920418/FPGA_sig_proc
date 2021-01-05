80fffc13ee6cf7c4ab42225fc763f9677d7f95d2
«Luis» <lptelecom92@gmail.com>
2021-01-05 22:39:32 +0100

    sript files compatible with matlab/octave to test the filters, fill up the reference.h with correct filtered values, ...

---
+++ octave_scripts/filter_test.m
@@ -0,0 +1,38 @@
+ pkg load signal
+a=1; %fir filter so a is zero always
+
+imag_part = [];
+%imag part, right side of reference
+for i=0:49
+  imag_part= [imag_part mod(i,10)];
+endfor
+
+real_part = 0:49; %left side of reference
+
+ b_std=[10,22,31,31,22,10];
+out_std_imag= filter(b_std,a,imag_part);
+out_std_real= filter(b_std,a,real_part);
+%f
+
+%---------------------------------------------------------------------
+ b_sym=[10,22,31,31,22,10];
+out_sym_imag= filter(b_std,a,imag_part);
+
+out_sym_real= filter(b_std,a,real_part);
+%---------------------------------------------------------------------
+
+
+
+amp=[1 1 0 0];
+f=[0 0.25 0.75 1];
+
+
+b_poly = remez(10, f,amp);  //in matlab use function firpm, in octave use function remez
+b_poly = round(2^10*b_poly');%make it signed 11 bits
+
+out_poly_1_imag=  round(upfirdn (imag_part, b_poly, 1, 2)/2^10);  %filter, downsample by two, divide by 2^10 to set unitary gain and round
+out_poly_1_real=  round(upfirdn (real_part, b_poly, 1, 2)/2^10); 
+
+
+
+refvector =  create_ref(out_poly_1_real,out_poly_1_imag);
