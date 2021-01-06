1db41a69c2391a995155387ec98dbcbfef6957a9
Bobinson <rob.will.robinson@gmail.com>
2021-01-06 17:44:29 +0100

    poly 3 test bench

--- vivado_project/design/polyphase/polyphase_3/tb_poly_3.cpp
+++ vivado_project/design/polyphase/polyphase_3/tb_poly_3.cpp
@@ -21,8 +21,26 @@
 #include "reference.h"
 
 using namespace sp_poly_3;
+using namespace std;
 
 int main(int argc, char **argv)
 {
+		hls::stream<t_in>  in;
+		hls::stream<t_out> out;
+
+		for ( int i = 0; i < 50; i += 1 ) {
+			t_in d(i,i%10);
+			in.write(d);
+		}
+
+		while(in.size())  
+		poly_3(in,out);
+
+		cout << "\n\n[FILTER POLY_3]\n\n";
+
+		c_compare cmp;
+	return cmp.chan(out,ref_poly_3);
+
+}
 	// =========================================================================
 
